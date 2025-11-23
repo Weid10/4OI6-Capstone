cfb031f54dd85fb2290f9c3352ba6bee55b71f20
RPi <RPi@RPi.ca>
2025-11-23 00:41:40 -0500

    capture image to array

--- capture.py
+++ capture.py
@@ -1,15 +1,32 @@
 from picamera2 import Picamera2
 import time
 
-OUTPUT_PATH = "samples"
-OUTPUT_NAME = "test.jpg"
 
-def take_photo():
+WIDTH = 1980
+HEIGHT = 1080
+
+
+def take_photo(save_location="samples/test.jpg"):
+    # Use Picamera2 to take a photo and save it to OUTPUT_PATH/OUTPUT_NAME
+    # https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
+
     picam2 = Picamera2()
+
     camera_config = picam2.create_still_configuration()
+    camera_config['main']['format'] = 'RGB888'
+    # camera_config['main']['size'] = (WIDTH, HEIGHT)
     picam2.configure(camera_config)
     picam2.start()
+
     time.sleep(2)
-    picam2.capture_file(f"{OUTPUT_PATH}/{OUTPUT_NAME}")
+
+    array = None
+    if save_location is None:
+        array = picam2.capture_array()
+    else:
+        picam2.capture_file(save_location)
+        print(f"Photo taken and saved to {save_location}")
+
     picam2.stop()
-    print(f"Photo taken and saved to {OUTPUT_PATH}/{OUTPUT_NAME}")
+
+    return array
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import cv2 as cv
 import capture
 
+SAVE_PHOTO = False
+
 def main():
-    capture.take_photo()
+    if SAVE_PHOTO:
+        capture.take_photo("samples/saved_photo.jpg")
+    else:
+        array = capture.take_photo(None)
 
-
+        cv.imshow("Captured Photo", array)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
